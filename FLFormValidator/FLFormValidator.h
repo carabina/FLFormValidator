c082a3bf3f65ef7f09d95a559abb95aa80614a60
Vaibhav <vaibhav@nickelfox.com>
2017-07-01 12:26:50 +0530

    Initial Commit

---
+++ FLFormValidator/FLFormValidator.h
@@ -0,0 +1,19 @@
+//
+//  FLFormValidator.h
+//  FLFormValidator
+//
+//  Created by Vaibhav Parmar on 01/07/17.
+//  Copyright © 2017 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLFormValidator.
+FOUNDATION_EXPORT double FLFormValidatorVersionNumber;
+
+//! Project version string for FLFormValidator.
+FOUNDATION_EXPORT const unsigned char FLFormValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLFormValidator/PublicHeader.h>
+
+
